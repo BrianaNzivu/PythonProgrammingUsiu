5ba9c969955d775d2b01bc3949a567c8ec2ec144
Briana M. Nzivu <bnzivu@usiu.ac.ke>
2022-05-24 10:13:54 +0300

    Pound to kg converter

---
+++ poundtokg.py
@@ -0,0 +1,10 @@
+"""
+File : poundtokg.py
+This program converts punds to kgs
+Input : Weight in pounds
+Output : Weight in kgs
+"""
+
+weight = float(input("Enter your weight in pounds:"))
+convertkg = weight/2.2
+print(convertkg)
