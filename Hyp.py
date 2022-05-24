2b026b006a318920e839c7cbb264022140e33d2c
Briana M. Nzivu <bnzivu@usiu.ac.ke>
2022-05-24 09:20:33 +0300

    Calculating hyp

---
+++ Hyp.py
@@ -0,0 +1,5 @@
+from math import *
+length = float(input("What is the length?"))
+width = float(input("What is the width?"))
+hyp = sqrt(length ** 2 + width ** 2)
+print (hyp)
---
+++ area.py
@@ -0,0 +1,4 @@
+pie = 3.14
+radius = int(input("What is the radius of your circle?"))
+area = pie * radius ** 2
+print (area)
