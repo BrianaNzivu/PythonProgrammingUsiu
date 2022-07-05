74e83311211b1475f4f840ff75c0e1325c64f10c
BrianaNzivu <bjnzivu@gmail.com>
2022-07-05 10:04:31 +0300

    Starting functions

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 """
 Introduction to functions
 """
+# Multiplies two numbers
 def multiply (n, m):
     return n * m
 
@@ -10,6 +11,7 @@ c = multiply(a, b)
 
 print(c)
 
+# Performs arithmetic expressions on the value n
 x = int(input("Enter a number [1 - 100] :"))
 def dosomething (n):
     n = n * 2
@@ -21,3 +23,15 @@ if x < 1 or x >  100:
     print("Enter a number between 1 - 100")
 else:
    print(dosomething(x))
+
+# Encryter
+def noVowels (message):
+    vowels = "aeiou"
+    secret = ""
+    for letter in message:
+        if not letter in vowels:
+            secret = secret + letter
+    return secret
+
+your_message = input("Enter message: ")
+print("Your secret message is", noVowels(your_message))
