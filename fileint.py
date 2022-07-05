c46d74b3b1f30c92bdbc4f3d2cbd742827eb59e8
BrianaNzivu <bjnzivu@gmail.com>
2022-07-05 09:35:46 +0300

    Starting functions

--- fileint.py
+++
@@ -1,8 +0,0 @@
-filename =  input("Enter a file name: ")
-myfile = open(filename, "w")
-
-for i in range (1, 11):
-    myfile.write(str(i) + "")
-
-myfile.write("\n The end")
-myfile.close()
---
+++ functions.py
@@ -0,0 +1,11 @@
+"""
+Introduction to functions
+"""
+def multiply (n, m):
+    return n * m
+
+a = 5
+b = 6
+c = multiply(a, b)
+
+print(c)
