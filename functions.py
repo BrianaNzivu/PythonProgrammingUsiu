3692c093844079d5ffd97e026721803dcb66c0b6
BrianaNzivu <bjnzivu@gmail.com>
2022-07-05 09:42:33 +0300

    Starting functions

--- functions.py
+++ functions.py
@@ -9,3 +9,15 @@ b = 6
 c = multiply(a, b)
 
 print(c)
+
+x = int(input("Enter a number [1 - 100] :"))
+def dosomething (n):
+    n = n * 2
+    n = n ** 3
+    n = n/5
+    return n
+
+if x < 1 or x >  100:
+    print("Enter a number between 1 - 100")
+else:
+   print(dosomething(x))
