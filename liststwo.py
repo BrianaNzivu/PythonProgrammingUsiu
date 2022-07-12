0162aef8e2f0eaf49d92ba12de3aff379e7f3a56
BrianaNzivu <bjnzivu@gmail.com>
2022-07-12 10:39:17 +0300

    Update lists

---
+++ liststwo.py
@@ -0,0 +1,12 @@
+"""
+Other functions of lists
+"""
+
+numbers = [9, 23, 12, 10, 6, 11]
+
+for index in range(len(numbers)):
+    numbers[index] = numbers[index] * 2
+print(numbers)
+
+for v in zip(numbers):
+    print(*v)
