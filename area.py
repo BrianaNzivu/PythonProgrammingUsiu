7a46f9b090f1c646b30474a7aa2abf49c519670c
BrianaNzivu <bjnzivu@gmail.com>
2022-07-12 10:24:09 +0300

    Update area of a circle

--- area.py
+++ area.py
@@ -1,4 +1,17 @@
-pie = 3.14
-radius = int(input("What is the radius of your circle?"))
-area = pie * radius ** 2
-print (area)
+"""
+Area of a circle
+"""
+from math import *
+
+radius = input("What is the radius of your circle?")
+
+if radius[0] == "-" and radius[1:].isnumeric():
+    print("Error: Enter a number >= 0")
+
+elif not radius.isnumeric():
+    print("Enter a number not a letter")
+
+else:
+    radius = int(radius)
+    area = pi * radius ** 2
+print("Area of the circle is", area)
--- lists.py
+++ lists.py
@@ -1,7 +1,26 @@
+"""
+Addition in lists
+"""
+
 a = [11, 21, 36]
 
+#Way one
 total = 0
 for i in range(len(a)):
     total += a[i]
 
 print("the total is", total)
+
+#Way two
+totalTwo = 0
+for index in a:
+    totalTwo = totalTwo + index
+print("the total is", totalTwo)
+
+#Way three
+def add(x, y):
+    return x + y
+
+print("The total is", sum(a))
+
+print(add(5, 9))
