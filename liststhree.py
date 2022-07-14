274947369ffed5d9f7fe80d9e2a6701e92af826b
BrianaNzivu <bjnzivu@gmail.com>
2022-07-14 09:47:29 +0300

    Pattern matching in lists

--- liststhree.py
+++ liststhree.py
@@ -1,26 +1,41 @@
+# """
+# Increments in lists
+# """
+#
+# numbers = [5, 9, 12, 1, 8]
+# print(numbers)
+#
+# # index based for loop to process list
+# for i in range(len(numbers)):
+#     numbers[i] = numbers[i] + 1
+# print(numbers)
+#
+# # item based for loop to process list
+# for items in numbers:
+#     items = items * 2
+#
+# print(numbers)
+# print()
+#
+# """
+# Splitting in lists
+# """
+# s = "Python is cool"
+# lst = s.split()
+# print(lst)
+
 """
-Increments in lists
+Pattern matching
 """
+fileName = input("Enter the file name:")
+inputFile = open(fileName, "r")
+highGrade = 0
+topStudent = "Nobody"
 
-numbers = [5, 9, 12, 1, 8]
-print(numbers)
-
-# index based for loop to process list
-for i in range(len(numbers)):
-    numbers[i] = numbers[i] + 1
-print(numbers)
-
-# item based for loop to process list
-for items in numbers:
-    items = items * 2
-
-print(numbers)
-print()
-
-"""
-Splitting in lists
-"""
-s = "Python is cool"
-lst = s.split()
-print(lst)
-
+for line in inputFile:
+    name, grade = line.split()
+    grade = int(grade)
+    if grade > highGrade:
+        highGrade = grade
+        topStudent = name
+print(topStudent, "has a grade of", highGrade)
