381c6cf412cf1dd5739fe2705fd9ed5b26433c68
BrianaNzivu <bjnzivu@gmail.com>
2022-08-01 19:00:18 +0300

    Updating Assignmentthree.py

---
+++ practicelist.py
@@ -0,0 +1,17 @@
+'''
+Practicing lists for finals
+'''
+
+# Incremet
+data = [1, 3, 2, 9, 5]
+
+# for index in range (len(data)):
+#     data[index] = data[index] + 1
+#
+# print(data)
+
+# largest number
+for large in data:
+    data.sort()
+large = data[-1]
+print(large)
